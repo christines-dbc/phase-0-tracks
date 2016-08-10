696163da114e98fed47eb364ca3e674144cad4d7
christine <ckduane@gmail.com>
2016-08-09 17:41:46 -0700

    finish release 1

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,2 +1,21 @@
-my_array = []
-p my_array
+cat_rappers = []
+p cat_rappers
+
+cat_rappers += ["DJ Catnip", "Meow Mix", "Fluffy D", "Lil Klaw", "Feline Factory"]
+p cat_rappers
+
+cat_rappers.delete_at(2)
+p cat_rappers
+
+cat_rappers.insert(2, "2Tail")
+p cat_rappers
+
+cat_rappers.shift
+p cat_rappers
+
+p "DJ Catnip is one of the top five cat rappers: #{cat_rappers.include?("DJ Catnip")}"
+
+dog_rappers = ["Lil Bow Wow", "Barky McJones", "Milkbone"]
+
+animal_rappers = cat_rappers + dog_rappers
+p animal_rappers
