f4837108e6fce203f90fd4fe2f56705cde0e86bb
christine <ckduane@gmail.com>
2016-08-10 15:01:36 -0700

    finish release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,31 @@ def meals
 end
 
 meals { |meal| puts "What did you have for #{meal}?"}
+
+smoothie = ["banana", "cashew milk", "strawberries"]
+
+common_traits = {"dog" => "loyal", "cat" => "obnoxious", "parrot" => "noisy"}
+
+puts smoothie
+
+smoothie.each do |ingredient|
+	puts ingredient
+end
+
+puts smoothie
+
+puts common_traits
+
+common_traits.each do |pet, trait|
+	puts "A #{pet} is #{trait}."
+end
+
+puts common_traits
+
+puts smoothie
+
+smoothie.map! do |ingredient|
+	ingredient.upcase
+end
+
+puts smoothie
