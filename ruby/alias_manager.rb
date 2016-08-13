b49e552156a565efc8035b8380c1c32265c8353e
christine <ckduane@gmail.com>
2016-08-13 16:25:19 -0700

    finish release 0

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,19 @@
-def name_swap(full_name)
-	full_name = full_name.split(" ")
-	full_name[1] + " " + full_name[0]
-end
+# def name_swap(full_name)
+# 	full_name = full_name.split(" ")
+# 	full_name[1] + " " + full_name[0]
+# end
 
 
 def next_letter(name)
+	# Take the first and last name and swap them
+	name = name.split(" ")
+	name = name[1] + " " + name[0]
+
 	# Create vowel and consonant variables
 	vowels = "aeiou".chars
 	consonants = "bcdfghjklmnpqrstvwxyz".chars
 
+	# Loop through each letter and change them
 	name_array = name.downcase.chars
 	name_array.map! do |letter|
 		if letter == " "
@@ -19,6 +24,13 @@ def next_letter(name)
 			letter = consonants[consonants.index(letter) + 1]
 		end
 	end
+
+	# Join all the letters
+	# Split them by the ' '
+	reversed_name = name_array.join.split(" ")
+	# Capitalize the first letter in the array and join again
+	reversed_name.map(&:capitalize).join(" ")
+
 end
 
 p next_letter("Felicia Torres")
