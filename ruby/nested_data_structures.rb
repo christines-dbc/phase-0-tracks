0e9ece2e6b6e4f13b2f11fb1a7194f81c460d6fe
christine <ckduane@gmail.com>
2016-08-13 11:48:15 -0700

    create nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,50 @@
+chaparral = {
+	plants: {
+		trees: [
+			"California Box Elder",
+			"White Alder",
+			"Cypress",
+			"Scrub Oak"
+		],
+		shrubs: [
+			"Sagebrush",
+			"Manzanita",
+			"Chamise",
+			"Copper Leaf"
+		],
+		flowers: [
+			"Sand Verbena",
+			"Yarrow",
+			"Mountain Dandelion",
+			"Snapdragon",
+			"Milkweed",
+			"Morning Glory"
+		]
+	},
+	animals: {
+		birds: [
+			"Wren",
+			"Kestrel",
+			"Hawk",
+			"Hummingbird",
+			"Quail"
+		],
+		small_mammals: [
+			"Squirrel",
+			"Rabbit",
+			"Bat",
+			"Mouse",
+			"Gopher"
+		],
+		medium_mammals: [
+			"Raccoon",
+			"Bobcat",
+			"Coyote"
+		],
+		large_mammals: [
+			"Deer",
+			"Mountain Lion",
+			"Bear"
+		]
+	}
+}
