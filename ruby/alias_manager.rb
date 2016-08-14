32ef633d74fc59ba0843490863a75d74d294bf27
christine <ckduane@gmail.com>
2016-08-13 17:24:09 -0700

    add user input

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,10 +6,8 @@
 
 def next_letter(name)
 	# Take the first and last name and swap them
-	if name != "quit"
-		name = name.split(" ")
-		name = name[1] + " " + name[0]
-	end
+	name = name.split(" ")
+	name = (name[1] + " " + name[0])
 
 	# Create vowel and consonant variables
 	vowels = "aeiou".chars
@@ -17,34 +15,39 @@ def next_letter(name)
 
 	# Loop through each letter and change them
 	name_array = name.downcase.chars
-	if name != "quit"
-		name_array.map! do |letter|
-			if letter == " "
-				letter = " "
-			elsif vowels.include? letter
-				letter = vowels[vowels.index(letter) + 1]
-			else consonants.include? letter
-				letter = consonants[consonants.index(letter) + 1]
-			end
+	name_array.map! do |letter|
+		if letter == " "
+			letter = " "
+		elsif vowels.include? letter
+			letter = vowels[vowels.index(letter) + 1]
+		else consonants.include? letter
+			letter = consonants[consonants.index(letter) + 1]
 		end
-
-		# Join all the letters
-		# Split them by the ' '
-		reversed_name = name_array.join.split(" ")
-		# Capitalize the first letter in the array and join again
-		reversed_name.map(&:capitalize).join(" ")
-	else
-		"Thank you"
 	end
 
-
-
+	# Join all the letters
+	# Split them by the ' '
+	reversed_name = name_array.join.split(" ")
+	# Capitalize the first letter in the array and join again
+	reversed_name.map(&:capitalize).join(" ")
 end
 
+spy_hash = {}
 received_name = ""
 
-until received_name == "quit"
+loop do
 	puts "What is your name?"
 	received_name = gets.chomp
-	p next_letter(received_name)
+
+	if received_name == "quit"
+		break
+	else
+	spy_name = next_letter(received_name)
+	spy_hash = {
+		received_name.to_sym => spy_name
+	}
+	end
 end
+
+# "#{key}'s spy name is #{value}"
+p spy_hash
