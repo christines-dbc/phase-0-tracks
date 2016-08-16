5c04ede06ac4a99f9be6433558ba288dd088a37a
christine <ckduane@gmail.com>
2016-08-16 14:28:55 -0700

    add gender and ethnicities

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 		puts "Initializing Santa instance ..."
 	end
 
@@ -9,6 +13,15 @@ class Santa
 	end
 
 	def eat_milk_and_cookies(cookie)
-		"That was a good #{cookie}"
+		puts "That was a good #{cookie}"
 	end
+end
+
+santas = []
+genders = ["gender non-conforming", "agender", "female", "gender fluid"]
+ethnicities = ["korean", "irish", "biracial", "asian"]
+
+genders.length.times do |i|
+	santas << Santa.new(genders[i], ethnicities[i])
+	puts "The new santa is #{genders[i]} and #{ethnicities[i]}"
 end
