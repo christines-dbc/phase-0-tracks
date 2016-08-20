69b2dadf064348d43a04911ac066b26618cd1019
christine <ckduane@gmail.com>
2016-08-20 14:20:51 -0700

    finish release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     "If you're happy and you know it, yell '#{words}'!!!"
+#   end
+# end
+
+# p Shout.yell_angrily("You're a wizard")
+# p Shout.yelling_happily("I'm a wizard")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def repeat(words)
+    p (words + "!! ") * 5
   end
 
-  def self.yelling_happily(words)
-    "If you're happy and you know it, yell '#{words}'!!!"
+  def louder(words)
+    p words.upcase
   end
 end
 
-p Shout.yell_angrily("You're a wizard")
-p Shout.yelling_happily("I'm a wizard")
+class Seagull
+  include Shout
+end
+
+class Sealion
+  include Shout
+end
+
+gull = Seagull.new
+gull.repeat("Mine")
+gull.louder("hello")
+
+sealion = Sealion.new
+sealion.repeat("Arf")
+sealion.louder("honk")
