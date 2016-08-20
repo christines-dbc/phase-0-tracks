8ee7c22a73c1e4b001d02edadf1bbc00d9f5af50
christine <ckduane@gmail.com>
2016-08-20 14:01:21 -0700

    finish release 0

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
 
+  def self.yelling_happily(words)
+    "If you're happy and you know it, yell '#{words}'!!!"
+  end
 end
+
+p Shout.yell_angrily("You're a wizard")
+p Shout.yelling_happily("I'm a wizard")
