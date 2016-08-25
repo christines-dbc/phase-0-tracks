df5d824b50e7c77097a992999a179348aca439a4
christine <ckduane@gmail.com>
2016-08-25 12:50:08 -0700

    finish algorithm and add driver code

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,14 @@
 // take the last letter of the string
 // add that letter to the new string
 // continue with other letters
+
+function reverse(string) {
+  var reversedString = "";
+  for (var i = string.length - 1; i >= 0; i--){
+    reversedString += string[i]
+  }
+  console.log(reversedString);
+}
+
+reverse("Hello");
+
