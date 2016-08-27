dd55df7163fa47b7378c44885d78d15fc5141b9b
christine <ckduane@gmail.com>
2016-08-27 11:40:40 -0700

    finish release 0

--- js/algos.js
+++ js/algos.js
@@ -1,19 +1,18 @@
 // create a function that takes an array of phrases
 // takes the length of each of the phrases
-// pair the phrase length with the phrase
-// return the phrase with the most letters
 // keep if the current phrase is longer than the previous phase
-// else keep previous phrase
 
 function longestPhrase(phrases) {
-  for (var i = phrases.length - 1; i > 0; i--)
-    if (phrases[i].length > phrases[i - 1].length){
-      console.log(phrases[i]);
-    } else {
-      console.log(phrases[i - 1]);
+  longerPhrase = "";
+  for (var i = phrases.length - 1; i >= 0; i--)
+    if (phrases[i].length > longerPhrase.length) {
+      longerPhrase = phrases[i];
     }
-}
+  console.log(longerPhrase);
+ }
+
 
 longestPhrase(["long phrase","longest phrase","longer phrase"])
 longestPhrase(["hello guess what", "this is the longest phrase", "or actually, its really this one hehehe"])
-longestPhrase(["you", "yo", "y"])
+longestPhrase(["i really think i figured this one out", "is this real"])
+longestPhrase(["no", "way", "this is amazing", "it was such a small change", "i did it"])
