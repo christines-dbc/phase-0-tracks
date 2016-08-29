aa3e2dbd847b1166c7704215d8999dfb7736bcda
christine <ckduane@gmail.com>
2016-08-28 20:05:51 -0700

    working on release 2

--- js/algos.js
+++ js/algos.js
@@ -12,7 +12,18 @@ function longestPhrase(phrases) {
  }
 
 
-longestPhrase(["long phrase","longest phrase","longer phrase"])
-longestPhrase(["hello guess what", "this is the longest phrase", "or actually, its really this one hehehe"])
-longestPhrase(["i really think i figured this one out", "is this real"])
-longestPhrase(["no", "way", "this is amazing", "it was such a small change", "i did it"])
+// longestPhrase(["long phrase","longest phrase","longer phrase"])
+// longestPhrase(["hello guess what", "this is the longest phrase", "or actually, its really this one hehehe"])
+// longestPhrase(["i really think i figured this one out", "is this real"])
+// longestPhrase(["no", "way", "this is amazing", "it was such a small change", "i did it"])
+
+// if obj1 key/value is similar to obj2 key/value return true
+function sharePair(obj1, obj2) {
+  if (Object.keys(obj1) == Object.keys(obj2)) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
+
+sharePair({name: "Steven", age: 54}, {name: "Tamir", age: 54})
