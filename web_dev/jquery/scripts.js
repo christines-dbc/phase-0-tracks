0c1a30debd923e26a61048803618992a7f0ce5f4
christine <ckduane@gmail.com>
2016-09-09 17:57:04 -0700

    working on jquery

--- web_dev/jquery/scripts.js
+++ web_dev/jquery/scripts.js
@@ -2,14 +2,13 @@ console.log("Testing!")
 
 $(document).ready(function() {
   $("img").click(function(event) {
-    alert("Woops, was that supposed to be a link? Maybe to a cat-only pizza shop?");
+    alert("Woops, was that supposed to be a link? Maybe to a cat-only pizza shop? But definitely not a pop up. Who uses pop ups?");
   });
   $("aside").click(function(event){
     event.preventDefault();
     $(this).hide("slow");
-    
+    $(this).show("slow");
   });
-
 });
 
 
